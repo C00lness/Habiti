76f506fd0550f748c6a35af9dded653c8752da63
Ermek I. <Directoryourlabel@gmail.com>
2026-03-06 21:20:34 +0300

    Обновление: .gitignore, настройки IDEA и модуль Habiti

--- app/src/main/java/com/habiti/app/HabitiApp.kt
+++ app/src/main/java/com/habiti/app/HabitiApp.kt
@@ -1,13 +1,12 @@
 package com.habiti.app
 
 import android.app.Application
+import org.koin.core.context.startKoin
 
 class HabitiApp: Application() {
     override fun onCreate() {
         super.onCreate()
-//        startKoin {
-//            androidContext(this@HabitiApp)
-//            modules(coreModule, HabitsModule)
-//        }
+        startKoin {
+        }
     }
 }
